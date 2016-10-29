f50224ba28e5e32562942c4f4cccf980e23ac579
wayz-of-chang <david@dchang.no-ip.org>
2016-10-29 10:00:36 -0500

    added templates and static files

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
-from flask import Flask
+# [START app]
+import logging
+
+from flask import Flask, render_template, request
 app = Flask(__name__)
 app.config['DEBUG'] = True
 
@@ -11,8 +14,31 @@ def hello():
     """Return a friendly HTTP greeting."""
     return 'Hello World!'
 
+@app.route('/form')
+def form():
+    return render_template('form.html')
+
+@app.route('/submitted', methods=['POST'])
+def submitted_form():
+    name = request.form['name']
+    email = request.form['email']
+    site = request.form['site_url']
+    comments = request.form['comments']
+    return render_template('submitted_form.html',
+        name=name,
+        email=email,
+        site=site,
+        comments=comments
+    )
 
 @app.errorhandler(404)
 def page_not_found(e):
     """Return a custom 404 error."""
     return 'Sorry, nothing at this URL.', 404
+
+@app.errorhandler(500)
+def server_error(e):
+    """Return a custom 404 error."""
+    logging.exception('An error occurred during a request.')
+    return 'An internal error occurred.', 500
+# [END app]
